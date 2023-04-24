947120ba0d3b65caf2f5411a509280916f52ddb9
param-ninthu <ninthu1999@gmail.com>
2023-04-24 13:52:03 +0530

    Create userprofile

--- src/main/java/routeradar/rrviewer/model/UserProfile.java
+++ src/main/java/routeradar/rrviewer/model/UserProfile.java
@@ -1,4 +1,25 @@
 package routeradar.rrviewer.model;
 
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class UserProfile {
+    protected ObjectId id;
+    protected String fullName;
+    protected String nic;
+    protected String company;
+    protected String phoneNo;
+    protected String secondaryContact;
+    protected String residentialAddress;
+    protected String officeAddress;
+    protected String companyId;
+    protected String email;
+    protected List<String> contactNos = new ArrayList<>();
+    protected Date registeredAt;
+    protected Date updatedAt;
+    protected boolean isActive;
+    protected boolean status;
 }
